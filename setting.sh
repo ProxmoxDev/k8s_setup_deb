2fab8ff2602f401fbb8fd43730f41a245b761a9c
Syun Tanaka <42093052+piyoraik@users.noreply.github.com>
2024-05-14 17:57:34 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -3,12 +3,12 @@
 PRIVATE_IP_ADDRESS=192.168.100.
 DEFAULT_GATEWAY=192.168.100.1
 
-cat <<EOF > /etc/netplan/00_static_ip_config.yaml
+cat <<EOF > /etc/netplan/99_static_ip_config.yaml
 network:
   version: 2
   renderer: networkd
   ethernets:
-    ens18::
+    ens18:
       dhcp4: false
       dhcp6: true
       addresses:
@@ -20,5 +20,5 @@ network:
        addresses: [8.8.8.8,1.1.1.1]
 EOF
 
-chmod 600 /etc/netplan/00_static_ip_config.yaml
+chmod 600 /etc/netplan/99_static_ip_config.yaml
 netplan apply
