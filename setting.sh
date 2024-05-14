0773e15170dbc1177027a86fdc5dd0ef33e3a239
Syun Tanaka <42093052+piyoraik@users.noreply.github.com>
2024-05-14 17:48:32 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+PRIVATE_IP_ADDRESS=192.168.100.
+DEFAULT_GATEWAY=192.168.100.1
+
 cat <<EOF > /etc/netplan/00_static_ip_config.yaml
 network:
   version: 2
   renderer: networkd
   ethernets:
-    enp0s3:
+    ens18::
       dhcp4: false
       dhcp6: true
       addresses:
-      - 192.168.100.122/24
+      - ${PRIVATE_IP_ADDRESS}/24
       routes:
       - to: default
-        via: 192.168.100.1
+        via: ${DEFAULT_GATEWAY}
       nameservers:
        addresses: [8.8.8.8,1.1.1.1]
 EOF
